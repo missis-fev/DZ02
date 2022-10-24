4ca5eec98a828400c75faf60bf6c40a30265e7bc
Elena Frolova <missis-fev@yandex.ru>
2022-10-24 13:23:15 +0300

    Task10 решение

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+Console.WriteLine("Программа, на выходе показывает вторую цифру введенного трехзначного числа");
+Console.WriteLine("Введите число: ");
+int Digit = int.Parse(Console.ReadLine()!);
+
+int OutSecondDigit(int Digit)
+{
+    int firstDigit = Digit/100;
+    int secondDigit = (Digit - firstDigit*100)/10;
+    int thirdDigit = Digit % 10;
+    //return (firstDigit*10+thirdDigit);
+    return (secondDigit);
+}
+
+Console.WriteLine(OutSecondDigit (Digit));
